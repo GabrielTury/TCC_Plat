c3c653f712f3b25dd02b54eb67a20cc31a4a7199
Gigamatts <mateusmtorres.17@gmail.com>
2025-05-28 22:22:46 -0300

    Temporary final collectible added

---
+++ Plat_TCC/Assets/GameplayAssets/S_RotateGear.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class S_RotateGear : MonoBehaviour
+{
+    [SerializeField]
+    private float rotate_speed, spin_speed, float_speed, height;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, Time.deltaTime * rotate_speed);
+        transform.Rotate(0, Time.deltaTime * spin_speed, 0,Space.World);
+
+        float float_height = Mathf.Sin(Time.time * float_speed) * height / 10;
+        Debug.Log(float_height);
+        transform.position = new Vector3(transform.position.x, transform.position.y + float_height, transform.position.z);
+
+    }
+}
