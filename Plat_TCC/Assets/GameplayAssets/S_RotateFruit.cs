0bf0d581b9e3c7ac8f2c7803dc506135066ab651
Gigamatts <mateusmtorres.17@gmail.com>
2025-05-31 11:34:37 -0300

    updated 1-1

---
+++ Plat_TCC/Assets/GameplayAssets/S_RotateFruit.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class S_RotateFruit : MonoBehaviour
+{
+    [SerializeField]
+    private float spin_speed, float_speed, height;
+
+    private void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * spin_speed, 0, Space.World);
+
+        float float_height = Mathf.Sin(Time.time * float_speed) * height / 10;
+        transform.position = new Vector3(transform.position.x, transform.position.y + float_height, transform.position.z);
+
+    }
+}
