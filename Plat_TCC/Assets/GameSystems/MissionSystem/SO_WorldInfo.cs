2ebec44fc1b5221dbe027fd4d2f959407e931ad5
Gabriel Mendes Tury Guimarães <80724636+GabrielTury@users.noreply.github.com>
2025-08-12 10:07:17 -0300

    Remove mission system and add level lighting info
    Deleted MissionCreatorTool, SO_MissionInfo, and S_Mission scripts and their meta files, removing the mission system. Updated SO_WorldInfo to include LevelLightInfo for per-mission lighting configuration. Modified World_0 and World_Test assets to add levelLightInfos. Adjusted AssetGallery scene light color and rotation.

--- Plat_TCC/Assets/GameSystems/MissionSystem/SO_WorldInfo.cs
+++ Plat_TCC/Assets/GameSystems/MissionSystem/SO_WorldInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,4 +8,19 @@ public class SO_WorldInfo : ScriptableObject
     public int worldId;
 
     public string[] missionSceneNames;
+
+    [Tooltip("Each light info assigned to the mission in the same index at Mission Scene Names")]
+    public LevelLightInfo[] levelLightInfos;
+}
+
+[Serializable]
+public struct LevelLightInfo
+{
+    public Color lightColor;
+
+    public float intensity;
+
+    public Quaternion directionalLightRotation;
+
+    public Material skybox;
 }
