e8665357f704f03041a23d210367eda31bfc3302
Castanha <69070402+Castanha@users.noreply.github.com>
2025-09-02 22:34:26 -0300

    Collection VFX Base + Apple Collect VFX

--- Plat_TCC/Assets/GameplayAssets/S_Collectible.cs
+++ Plat_TCC/Assets/GameplayAssets/S_Collectible.cs
@@ -3,17 +3,21 @@ using UnityEngine;
 public class S_Collectible : MonoBehaviour
 {
     [SerializeField] private AudioClip appleCollected;
+    [SerializeField] private GameObject collectVFXPrefab;
     [SerializeField] private int collectibleId = -1;
 
-    public void SetId(int id)
-    {
-        collectibleId = id;
-    }
+    public void SetId(int id) => collectibleId = id;
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.CompareTag("Player")) return;
+
         S_LevelManager.instance.AddCollectible("Apple", 1);
         AudioManager.instance.PlaySFX(appleCollected);
+
+        if (collectVFXPrefab != null)
+            Instantiate(collectVFXPrefab, transform.position, Quaternion.identity);
+
         Destroy(gameObject);
     }
 }
