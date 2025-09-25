b86e32e09a06fbc99cb59a9fb91b67593f957c7a
Gigamatts <mateusmtorres.17@gmail.com>
2025-09-25 10:57:46 -0300

    1-3 Camera added

--- Plat_TCC/Assets/GameSystems/CameraPerspective/S_ChangeCamPerspective.cs
+++ Plat_TCC/Assets/GameSystems/CameraPerspective/S_ChangeCamPerspective.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -7,9 +8,26 @@ public class S_ChangeCamPerspective : MonoBehaviour
     [SerializeField]
     private CinemachineCamera changeCam;
 
+    [SerializeField]
+    private Transform lookAtTarget;
+
+    [SerializeField]
+    private Transform playerTransform;
+
+
     private void Start()
     {
         changeCam.enabled = false;
+    }
+
+    private void LateUpdate()
+    {
+        // Move our position a step closer to the target.
+        var step = 100 * Time.deltaTime; // calculate distance to move
+
+        Vector3 nextPos = lookAtTarget.position;
+        nextPos.y = playerTransform.position.y;
+        lookAtTarget.position = Vector3.MoveTowards(lookAtTarget.position, nextPos, step);
     }
 
     private void OnTriggerEnter(Collider other)
