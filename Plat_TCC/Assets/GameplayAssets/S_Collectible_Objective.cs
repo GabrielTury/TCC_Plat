8ae4116d9f06b397568e216008e1f7c1ae2dbb60
Gigamatts <mateusmtorres.17@gmail.com>
2025-10-14 10:04:16 -0300

    Objective wait time

--- Plat_TCC/Assets/GameplayAssets/S_Collectible_Objective.cs
+++ Plat_TCC/Assets/GameplayAssets/S_Collectible_Objective.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class S_Collectible_Objective : MonoBehaviour
@@ -9,8 +10,18 @@ public class S_Collectible_Objective : MonoBehaviour
     {
         S_LevelManager.instance.AddCollectible("Main", 1);
         S_MissionManager.instance.SaveCurrentMissionStatus(true);
+        other.GetComponentInParent<S_PlayerMovement>().PausePlayer();
+        other.GetComponentInParent<Rigidbody>().linearVelocity = Vector3.zero;//@todo colocar um metodo de gente
+        //S_LevelManager.instance.playerMovement.PausePlayer(true, true);
         AudioManager.instance.PlaySFX(goalCollected);
-        Destroy(gameObject);
+        StartCoroutine(LevelChangeDelay());
+
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private IEnumerator LevelChangeDelay()
+    {
+        yield return new WaitForSeconds(3);
         S_TransitionManager.instance.GoToLevel("HubWorld");
     }
 }
